f909f83343811c5fd11bca6ace7feedd419435d3
Nikolay Ulanov <iamnickolasulanov@gmail.com>
2021-05-26 14:25:52 +0300

    add optional argument at script gendiff.py

--- gendiff.py
+++ gendiff.py
@@ -3,7 +3,13 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="Generate diff")
+
+# Positional args:
 parser.add_argument('first_file')
 parser.add_argument('second_file')
+
+# Optional args:
+parser.add_argument('-f', '--format', type=str, help='set format of output', default='JSON')
+
 args = parser.parse_args()
-print(args.first_file, args.second_file)
+

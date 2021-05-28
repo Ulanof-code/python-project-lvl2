4236454dcf6459603d32d81e856683a944a8d8f4
Nikolay Ulanov <iamnickolasulanov@gmail.com>
2021-05-28 16:08:46 +0300

    add func generate_diff to gendiff

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import json
 import argparse
 
 parser = argparse.ArgumentParser(description="Generate diff")
@@ -13,3 +14,24 @@ parser.add_argument('-f', '--format', type=str, help='set format of output', def
 
 args = parser.parse_args()
 
+
+def generate_diff(file1, file2):
+    data1 = json.load(open(file1))
+    data2 = json.load(open(file2))
+    result = '{\n'
+    sorted_keys = sorted(set(sorted(data1) + sorted(data2)))
+
+    for key in sorted_keys:
+        if key in data1 and key in data2:
+            if data1[key] == data2[key]:
+                result += '  {} {}: {} \n'.format(' ', key, data1[key])
+            else:
+                result += '  {} {}: {} \n'.format('-', key, data1[key])
+                result += '  {} {}: {} \n'.format('+', key, data2[key])
+        if key in data1 and key not in data2:
+            result += '  {} {}: {} \n'.format('-', key, data1[key])
+        if key in data2 and key not in data1:
+            result += '  {} {}: {} \n'.format('+', key, data2[key])
+    return result + '}'
+
+print(generate_diff(args.first_file, args.second_file))

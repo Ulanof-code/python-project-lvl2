3eb2e4998ea22ecd76c60ded3e232531201da99a
Nikolay Ulanov <iamnickolasulanov@gmail.com>
2021-06-04 23:43:18 +0300

    add fixtures and tests for complex files json/yaml

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,9 +1,11 @@
 from gendiff.gendiff import generate_diff
 from gendiff.parser import parse
 
-correct = ''
 with open('tests/fixtures/correct_output.txt', 'r') as file:
     correct = file.read()
+
+with open('tests/fixtures.correct_complex_output.txt', 'r') as file:
+    complex_correct = file.read()
 
 
 def test_correct_output_json():
@@ -20,3 +22,19 @@ def test_correct_output_yaml():
         parse('tests/fixtures/file2.yml')
     )
     assert tmp == correct
+
+
+def test_complex_json():
+    tmp = generate_diff(
+        parse('tests/fixtures/complex_file1.json'),
+        parse('tests/fixtures/complex_file2.json')
+    )
+    assert tmp == complex_correct
+
+
+def test_complex_yaml():
+    tmp = generate_diff(
+        parse('tests/fixtures/complex_file1.yaml'),
+        parse('tests/fixtures/complex_file2.yml')
+    )
+    assert tmp == complex_correct

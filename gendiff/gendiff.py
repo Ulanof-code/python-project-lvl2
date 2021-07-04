547071d7ee07bdd9e3b7fa282ef2bc8bc29fd324
Nikolay Ulanov <iamnickolasulanov@gmail.com>
2021-07-04 13:13:14 +0300

    Used constants

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -9,13 +9,24 @@ from gendiff.formatters.formats import FORMATS
 def generate_diff(first_file: str,
                   second_file: str,
                   format_output='stylish') -> str:
+    """
+    The formatter is selected in FORMATS: tuple, where [0] == 'stylish', [1] == 'plain', [2] == 'json'
+
+    Args:
+        first_file:
+        second_file:
+        format_output:
+
+    Returns:
+        Formatted output.
+    """
     dict1 = get_dict_from_file(first_file)
     dict2 = get_dict_from_file(second_file)
     diffs = make_diffs_representation(dict1, dict2)
     if format_output not in FORMATS:
         raise NotImplementedError(f"The format {format_output} is not supported")
-    elif format_output == 'plain':
+    elif format_output == FORMATS[1]:  # plain
         return make_plain(diffs)
-    elif format_output == 'json':
+    elif format_output == FORMATS[2]:  # json
         return make_json(diffs)
-    return make_stylish(diffs)
+    return make_stylish(diffs)  # stylish
